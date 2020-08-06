d96562506cfced503bf857ccbbe0f544bc00eeb8
SergeyMashkevich <SergeyMashkevich@users.noreply.github.com>
2020-08-07 01:12:58 +0200

    Update HangMan.js

--- HangMan.js
+++ HangMan.js
@@ -42,9 +42,9 @@
 
 alert('HANG-MAN GAME' + '\n' + 'My first \'game\' based on just: js code. ' + '\n' + 'Press \'ok\' to start.')
 if (confirm('Would you like to play with fewer chances to guess?' + '\n' + 'If you accept it click \'ok\'')) {
-    var lives = 5
+    var lives = 4
 } else {
-    var lives = 10
+    var lives = 6
 }
 var words = ['frog', 'mobile', 'animal', 'bread', 'program', 'monkey', 'grandmother', 'butter', 'telephone','hypertextmarkuplanguage','earth','friend','submarine','language','airplane','computer','warrior']
 var word = words[Math.floor(Math.random() * words.length)]
